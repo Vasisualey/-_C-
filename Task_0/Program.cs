24c4b1e7aa7bb64d9011e2514e979e84ed396b6c
*Vasisualey* <basilnekrasov@mail.ru>
2023-01-29 17:48:47 +0300

    dobavili papku Task_0

---
+++ Task_0/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()); // Из консоли приходит символьный(строковый) тип данных, переводим их int.parce
+int sqr = number * number;
+Console.WriteLine("Квадрат числа " + number + " равен " + sqr +"."); // автоформатирование происходит с помощью shift + alt + F
